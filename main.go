329ca57b70770bbd9745cbefa2ef23d7f52cbf08
Александр Мацуев <alex@MacBook-Pro-Aleksandr.local>
2020-07-05 01:32:17 +0700

    update main.go

--- main.go
+++ main.go
@@ -3,20 +3,20 @@ package main
 import (
 	"fmt"
 	"log"
-	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func main() {
 	// подключаемся к боту с помощью токена
-	bot, err := tgbotapi.NewBotAPI("1281304074:AAFXLPttlX0sm92QxHdTFlyacer9Hapf_Ic")
+	// bot, err := tgbotapi.NewBotAPI("1281304074:AAFXLPttlX0sm92QxHdTFlyacer9Hapf_Ic")
+	bot, err := tgbotapi.NewBotAPI("368741635:AAF_a7JaWZri88Hn4CJCqPkKZLq9UF49qA4")
 
 	if err != nil {
 		log.Panic(err)
 	}
 
-	// bot.Debug = true
+	bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
@@ -26,45 +26,86 @@ func main() {
 	updates, err := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		cmd := ""
+		var m *tgbotapi.Message
+		var msg tgbotapi.MessageConfig
 
 		if update.CallbackQuery != nil {
-			m := update.CallbackQuery.Message
-			// log.Println(update.CallbackQuery.Data)
+			m = update.CallbackQuery.Message
+			msg = tgbotapi.NewMessage(m.Chat.ID, "")
+
 			switch update.CallbackQuery.Data {
-			case "time":
-				cmd = "time"
+			case "begin":
+				msg.Text = "Выбери из списка предметы, по которым ты хотел бы решать задачи"
+				msg.ReplyMarkup = newPredmetKeyboard()
+				bot.Send(msg)
+			case "math":
+				msg.Text = "Математика"
+				bot.Send(msg)
+			case "phis":
+				msg.Text = "Физика"
+				bot.Send(msg)
+			case "it":
+				msg.Text = "Информатика"
+				bot.Send(msg)
 			}
+			continue
 		} else {
-			if update.Message == nil { // ignore any non-Message Updates
-				continue
-			}
-			m := update.Message
-			cmd = m.Command()
+			m = update.Message
+			msg = tgbotapi.NewMessage(m.Chat.ID, "")
+			msg.ParseMode = "markdown"
 		}
 
-		// log.Println(update.Message)
-
-		msg := tgbotapi.NewMessage(m.Chat.ID, "")
-
-		if cmd != "" {
+		if m.IsCommand() {
 			switch m.Command() {
 			case "start":
-				msg.Text = fmt.Sprintf("Привет, %s!", m.From.FirstName)
-				keyboard := tgbotapi.InlineKeyboardMarkup{}
-				var row []tgbotapi.InlineKeyboardButton
-				btn := tgbotapi.NewInlineKeyboardButtonData("Время в Красноярске", "time")
-				row = append(row, btn)
-				keyboard.InlineKeyboard = append(keyboard.InlineKeyboard, row)
-				msg.ReplyMarkup = keyboard
-			case "time":
-				t := time.Now()
-				msg.Text = fmt.Sprintf("Сейчас в Красноярске %d:%d:%d", t.Hour(), t.Minute(), t.Second())
+				msg.Text = fmt.Sprintf("*Привет, %s!*\n\nЗдесь ты можешь каждый день решать интересные задачи и зарабатывать дополнительные баллы для поступления в КЛШ-2021.", m.From.FirstName)
+				msg.ReplyMarkup = newWelcomeKeyboard()
+				bot.Send(msg)
+			case "key":
+				msg.ReplyMarkup = newKeyboard()
+				msg.Text = "Клавиатура"
+				bot.Send(msg)
+			case "photo":
+				photo := tgbotapi.NewPhotoUpload(m.Chat.ID, "a_d21fbd44.jpg")
+				m, e := bot.Send(photo)
+				fmt.Println(m)
+				fmt.Println(e)
 			}
+		} else if m.Text != "" {
+			fmt.Println()
+			fmt.Println(m.Chat)
+			fmt.Println()
+			msg.Text = "✔️ " + m.Text
+			bot.Send(msg)
 		} else {
-			msg.Text = m.Text
+			continue
 		}
-
-		bot.Send(msg)
 	}
+}
+
+func newKeyboard() (keyb tgbotapi.ReplyKeyboardMarkup) {
+	btn1 := tgbotapi.NewKeyboardButton("Button 1")
+	btn2 := tgbotapi.NewKeyboardButtonLocation("Location")
+	btn3 := tgbotapi.NewKeyboardButton("Button 3")
+	btn4 := tgbotapi.NewKeyboardButton("Button 4")
+	row1 := tgbotapi.NewKeyboardButtonRow(btn1, btn2)
+	row2 := tgbotapi.NewKeyboardButtonRow(btn3, btn4)
+	keyb = tgbotapi.NewReplyKeyboard(row1, row2)
+	return
+}
+
+func newWelcomeKeyboard() (keyb tgbotapi.InlineKeyboardMarkup) {
+	btn1 := tgbotapi.NewInlineKeyboardButtonData("Выбрать предметы", "begin")
+	row1 := tgbotapi.NewInlineKeyboardRow(btn1)
+	keyb = tgbotapi.NewInlineKeyboardMarkup(row1)
+	return
+}
+
+func newPredmetKeyboard() (keyb tgbotapi.InlineKeyboardMarkup) {
+	btn1 := tgbotapi.NewInlineKeyboardButtonData("Математика", "math")
+	btn2 := tgbotapi.NewInlineKeyboardButtonData("Физика", "phis")
+	btn3 := tgbotapi.NewInlineKeyboardButtonData("Информатика", "it")
+	row1 := tgbotapi.NewInlineKeyboardRow(btn1, btn2, btn3)
+	keyb = tgbotapi.NewInlineKeyboardMarkup(row1)
+	return
 }
